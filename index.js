bd6c3589ca6326d7bcf89c74e20a218379dade46
gliggy <leocfitz@gmail.com>
2021-12-26 11:04:12 -0500

    add error reporting

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-async function run() {
+// -*- mode: js; js-indent-level: 4; -*-
 
+async function run() {
+    
 // require stuff
 const { Client, Intents, MessageAttachment, MessageEmbed } = require('discord.js');
 const { token } = require('./config.json');
@@ -16,61 +18,74 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
-	//console.log(key);
+    console.log('Ready!');
+    //console.log(key);
 });
 
 async function sendPost(file,template,key) {
-	console.log("FILE IS", file);
+    console.log("FILE IS", file);
 
-	const body = new FormData();
-	body.append('images', file, 'file.jpg');
-	//console.log("body will be", {body: body._blob()});
-	var options = {
-		method: 'POST',
-		headers: {
-			// 'Content-Type': 'multipart/form-data',
-			'Authorization': key,
-			//'Content-Length': data.length
-		},
-		body
-	};
+    const body = new FormData();
+    body.append('images', file, 'file.jpg');
+    //console.log("body will be", {body: body._blob()});
+    var options = {
+	method: 'POST',
+	headers: {
+	    // 'Content-Type': 'multipart/form-data',
+	    'Authorization': key,
+	    //'Content-Length': data.length
+	},
+	body
+    };
 
-	// Set up the request
-	const host = "http://localhost:3000"
-	const text = "froggy"
-	const req = await fetch(host + `/make/${template}?text=${text}`, options);
-	//`https://api.makesweet.com/make/${template}?text=froggy`,
-	return req;
+    // Set up the request
+    const host = "https://api.makesweet.com"
+    const text = "froggy"
+    const req = await fetch(host + `/make/${template}?text=${text}`, options);
+    //`https://api.makesweet.com/make/${template}?text=froggy`,
+    return req;
 }
 
 const fileTest = fs.readFileSync('frog.jpg'); //, {encoding:'utf8'});
 
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
 
  // const result = await YOUR_FUNCTION();
    // await interaction.editReply(result);
-	const { commandName } = interaction;
+    const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'heart-locket') {
-	    await interaction.reply('Working on it');
-	    const img = await sendPost(fileTest,'heart-locket',key);
+    if (commandName === 'ping') {
+	await interaction.reply('Pong!');
+    } else if (commandName === 'heart-locket') {
+	await interaction.reply('Working on it');
+	const img = await sendPost(fileTest,'heart-locket',key);
+	console.log(img.status);
+	if (img.status === 200) {
 	    //const imgArray = await img.arrayBuffer();
 	    //console.log({imgArray});
 	    const file = new MessageAttachment(img.body, 'animation2.gif');
-		const gif = {
-			title: 'gif',
-			image: {
-				url: 'attachment://animation2.gif',
-			},
-		};
-		await interaction.editReply({ embeds: [gif], files: [file] });
+	    const gif = {
+		color: 0x006400,
+		title: 'gif',
+		image: {
+		    url: 'attachment://animation2.gif',
+		},
+	    };
+	    await interaction.editReply({ embeds: [gif], files: [file] });
+	} else {
+	    const error = (await img.json()).error
+	    const gif = {
+		color: 0xff0000,
+		title: 'Error',
+		description: `Error ${error} occured`,
+	    };
+	    await interaction.editReply({ embeds: [gif] });
+	}
+	    
 	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	    await interaction.reply('User info.');
 	}
 });
 
