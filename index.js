8334b896d6905f383db78bd85936ba974520148d
gliggy <leocfitz@gmail.com>
2021-12-26 10:05:03 -0500

    do stuff

--- index.js
+++ index.js
@@ -1,16 +1,82 @@
-const Discord = require('discord.js');
- const client = new Discord.Client();
+async function run() {
 
-client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
-
-client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
-
+// require stuff
+const { Client, Intents, MessageAttachment, MessageEmbed } = require('discord.js');
 const { token } = require('./config.json');
+const { key } = require('./config.json');
+const https = require('https');
+const fetch = require('node-fetch');
+const {File} = await import ('fetch-blob/file.js');
+const FormData = require('form-data');
+const querystring = require('querystring');
+const fs = require('fs');
 
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+	//console.log(key);
+});
+
+async function sendPost(file,template,key) {
+	console.log("FILE IS", file);
+
+	const body = new FormData();
+	body.append('images', file, 'file.jpg');
+	//console.log("body will be", {body: body._blob()});
+	var options = {
+		method: 'POST',
+		headers: {
+			// 'Content-Type': 'multipart/form-data',
+			'Authorization': key,
+			//'Content-Length': data.length
+		},
+		body
+	};
+
+	// Set up the request
+	const host = "http://localhost:3000"
+	const text = "froggy"
+	const req = await fetch(host + `/make/${template}?text=${text}`, options);
+	//`https://api.makesweet.com/make/${template}?text=froggy`,
+	return req;
+}
+
+const fileTest = fs.readFileSync('frog.jpg'); //, {encoding:'utf8'});
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+ // const result = await YOUR_FUNCTION();
+   // await interaction.editReply(result);
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'heart-locket') {
+	    await interaction.reply('Working on it');
+	    const img = await sendPost(fileTest,'heart-locket',key);
+	    //const imgArray = await img.arrayBuffer();
+	    //console.log({imgArray});
+	    const file = new MessageAttachment(img.body, 'animation2.gif');
+		const gif = {
+			title: 'gif',
+			image: {
+				url: 'attachment://animation2.gif',
+			},
+		};
+		await interaction.editReply({ embeds: [gif], files: [file] });
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// Login to Discord with your client's token
 client.login(token);
+
+}
+
+run().catch(e => console.error(e));
